644e1af53869212762dac625d2647bfe60a130b4
kulyaginv <kulyaginv@gmail.com>
2022-02-07 12:22:17 +0300

    first commit

---
+++ database/seeders/DatabaseSeeder.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Seeder;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        // \App\Models\User::factory(10)->create();
+        $viktor = new User(['name' => 'victor', 'email'=>'victor@victor.ru', 'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi']);
+        $nastya = new User(['name' => 'nastya', 'email'=>'nasty@victor.ru', 'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi']);
+        $viktor->save();
+        $nastya->save();
+    }
+}
---
+++ resources/views/home/index.blade.php
@@ -0,0 +1,1 @@
+<h2>HomeController Index</h2>
